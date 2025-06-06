c677a0666e1b5e1a63ab6c0c67b4e4c05c064b1c
dapt <diegodapt18@gmail.com>
2025-06-06 22:23:09 +0200

    adding updates support

--- routes/contacts.py
+++ routes/contacts.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, render_template, request
+from flask import Blueprint, redirect, render_template, request, url_for
 
 from models.contacts import Contact
 from utils.db import db
@@ -7,7 +7,7 @@ contacts = Blueprint('contacts', __name__)
 
 
 @contacts.route('/', methods=["GET"])
-def home():
+def index():
     contacts = Contact.query.all()
     return render_template('index.html', contacts=contacts)
 
@@ -15,23 +15,39 @@ def home():
 @contacts.route('/new', methods=['POST'])
 def add_contact():
     try:
-        username, email, phone = request.form.values()
-        contact = Contact(username, email, phone)
+        fullname, email, phone = request.form.values()
+        contact = Contact(fullname, email, phone)
         db.session.add(contact)
         db.session.commit()
-        return redirect('/')
+        return redirect(url_for('contacts.index'))
     except Exception:
-        return redirect('/', 501)
+        return redirect(url_for('contacts.index'), 501)
 
 
-@contacts.route('/update', methods=['POST'])
-def update():
-    return "<h1>update contact</h1>"
+@contacts.route('/update/<id>', methods=['GET', 'POST'])
+def update(id):
+    contact = Contact.query.get(id)
+    if not contact:
+        return "Contacto no encontrado", 404
+    if request.method == "GET":
+        contact = Contact.query.get(id)
+        return render_template('update.html', contact=contact)
+    else:
+        fullname, email, phone = request.form.values()
+        contact.fullname = fullname
+        contact.email = email
+        contact.phone = phone
+        db.session.commit()
+        return redirect(url_for('contacts.index'))
 
 
-@contacts.route('/delete', methods=['POST'])
-def delete():
-    return "<h1>delete contact</h1>"
+@contacts.route('/delete/<id>', methods=['GET', 'POST'])
+def delete(id):
+    contact = Contact.query.get(id)
+    print(contact)
+    db.session.delete(contact)
+    db.session.commit()
+    return redirect(url_for('contacts.indeclass="card"x'))
 
 
 @contacts.route('/about', methods=["GET"])
